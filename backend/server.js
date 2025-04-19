1757e60ce41f17657d24b36e90dcdc5f9cd5cf41
ka8761 <dikachinwankwo575@gmail.com>
2025-04-19 13:09:13 +0100

    gave a variable to the path.join

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,6 @@ app.use('/api', applicationRoutes);
 // }
 
 
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
- 
-
 const connectDB = async () => {
   try {
     await mongoose.connect(mongoDBLink);
@@ -51,9 +48,11 @@ connectDB();
 // });
 
 if (process.env.NODE_ENV === 'production') {
+  const buildPath = path.join(__dirname, '../frontend/build');
+  app.use(express.static(buildPath));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(buildPath, 'index.html'));
   });
 }
 
