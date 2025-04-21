4c254e598cb143ebcb585f1175da3ce43851892a
ka8761 <dikachinwankwo575@gmail.com>
2025-04-21 07:39:22 +0100

    stepped down to express 4

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+require('dotenv').config(); // Load environment variables from .env file
+console.log(process.env.NODE_ENV)
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth.js');
 const applicationRoutes = require('./routes/application.js');
 const path = require('path');
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,14 +30,32 @@ const connectDB = async () => {
 };
 
 connectDB();
-
 if (process.env.NODE_ENV === 'production') {
+  console.log('serve build from development server');
   const buildPath = path.join(__dirname, '../frontend/build');
+  console.log(buildPath);
   app.use(express.static(buildPath));
   app.get('/*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
   });
 }
+// // }
+// // First handle API routes
+
+// // Then handle static files
+// // Add this before your routes
+
+  
+  console.log('Development mode active');
+  
+  app.get('/', (req, res) => {
+    res.send('Development server is running');
+  });
+  
+  app.get('*', (req, res) => {
+    res.status(404).send('Route not found in development mode');
+  });
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
